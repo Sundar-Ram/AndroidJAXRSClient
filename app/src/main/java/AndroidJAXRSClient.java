ba4c7a0d28f84d823af2bbdc703b2a6a0933d3a3
Sundar <sundars@xyntekinc.com>
2015-10-16 14:21:56 -0400

    Succesfully created Android Client which communicates with RESTful WebAPI

---
+++ app/src/main/java/AndroidJAXRSClient.java
@@ -0,0 +1,5 @@
+/**
+ * Created by sundars on 10/15/2015.
+ */
+public class AndroidJAXRSClient {
+}
--- app/src/main/java/com/sundarsxyntekinc/androidjaxrsclient/MainActivity.java
+++ app/src/main/java/com/sundarsxyntekinc/androidjaxrsclient/MainActivity.java
@@ -2,16 +2,90 @@ package com.sundarsxyntekinc.androidjaxrsclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView jaxrs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Find the Text View
+        jaxrs = (TextView) findViewById(R.id.jaxrs);
+
+
+
     }
+
+    Thread thread = new Thread(new Runnable(){
+        @Override
+        public void run() {
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpGet request = new HttpGet(
+                        "http://10.1.16.44:8080/RESTful_Jersey_Hello_World/rest/hello");
+
+                //request.addHeader("Accept", "text/html");
+                //	request.addHeader("Accept", "text/xml");
+                request.addHeader("Accept", "text/plain");
+                HttpResponse response = httpclient.execute(request);
+                HttpEntity entity = response.getEntity();
+                Log.e("Response-=-=---=-=",response.getEntity().toString());
+                InputStream instream = entity.getContent();
+                String jaxrsmessage = read(instream);
+                jaxrs.setText(jaxrsmessage);
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        thread.start();
+    }
+
+
+
+    private static String read(InputStream instream) {
+        StringBuilder sb = null;
+        try {
+            sb = new StringBuilder();
+            BufferedReader r = new BufferedReader(new InputStreamReader(
+                    instream));
+            for (String line = r.readLine(); line != null; line = r.readLine()) {
+                sb.append(line);
+            }
+
+            instream.close();
+
+        } catch (IOException e) {
+        }
+        return sb.toString();
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
